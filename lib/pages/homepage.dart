663e089c79dafab640850f408a4c0d03e204d4bc
JohnGastone <johngastone11@gmail.com>
2024-06-18 15:46:31 +0300

    +++

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,6 +12,34 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFF354249),
+      appBar: AppBar(
+        backgroundColor: Color(0xFF283035),
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              SizedBox(
+                height: 60,
+                width: 60,
+                child: ClipOval(
+                  child: Image.asset(
+                    "./assets/p2.png",
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
