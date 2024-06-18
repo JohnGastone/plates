c0034cd6ff281253b938d31e28eeb2eb6e49c88c
JohnGastone <johngastone11@gmail.com>
2024-06-18 16:40:01 +0300

    +++

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -18,53 +18,65 @@ class _HomepageState extends State<Homepage> {
       backgroundColor: Color(0xFF354249),
       appBar: AppBar(
         backgroundColor: Color(0xFF283035),
-        title: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        title: SizedBox(
+          width: double.infinity,
+          child: Column(
             children: [
-              SizedBox(
-                height: 50,
-                width: 50,
-                child: ClipOval(
-                  child: Image.asset(
-                    "./assets/p2.png",
-                    height: 50,
-                    width: 50,
-                    fit: BoxFit.cover,
-                  ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: ClipOval(
+                        child: Image.asset(
+                          "./assets/p2.png",
+                          height: 50,
+                          width: 50,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment
+                          .start, // Aligns the children to the start horizontally
+                      mainAxisAlignment: MainAxisAlignment
+                          .center, // Aligns the children to the center vertically
+                      children: [
+                        Text(
+                          "Hi Bellinda",
+                          style: GoogleFonts.roboto(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                        Text(
+                          "All the most delicious things for you",
+                          style: GoogleFonts.roboto(
+                              fontSize: 14, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF354249),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Icon(
+                        Icons.menu,
+                        color: Color(0xFFC72931),
+                      ),
+                    )
+                  ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment
-                    .start, // Aligns the children to the start horizontally
-                mainAxisAlignment: MainAxisAlignment
-                    .center, // Aligns the children to the center vertically
-                children: [
-                  Text(
-                    "Hi Bellinda",
-                    style: GoogleFonts.roboto(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    "All the most delicious things for you",
-                    style:
-                        GoogleFonts.roboto(fontSize: 14, color: Colors.white),
-                  ),
-                ],
               ),
               Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                    color: Color(0xFF354249),
-                    borderRadius: BorderRadius.circular(15)),
-                child: Icon(
-                  Icons.menu,
-                  color: Color(0xFFC72931),
-                ),
+                height: 10,
+                width: double.maxFinite,
+                decoration: BoxDecoration(color: Color(0xFFC72931)),
               )
             ],
           ),
@@ -73,7 +85,106 @@ class _HomepageState extends State<Homepage> {
       body: Center(
         child: SingleChildScrollView(
           child: Column(
-            children: [],
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF303A40),
+                          borderRadius: BorderRadius.circular(18)),
+                      child: Center(
+                        child: Text(
+                          "NEW",
+                          style: GoogleFonts.bayon(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 80,
+                      width: 135,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF283035),
+                          borderRadius: BorderRadius.circular(18)),
+                      child: Center(
+                        child: Text(
+                          "SEASONAL",
+                          style: GoogleFonts.bayon(
+                              color: Color(0xFFC72931), fontSize: 23),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF303A40),
+                          borderRadius: BorderRadius.circular(18)),
+                      child: Center(
+                        child: Text(
+                          "POPULAR",
+                          style: GoogleFonts.bayon(
+                              color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 230,
+                      width: 170,
+                      decoration: BoxDecoration(color: Color(0xFF283035)),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Align(
+                              alignment: Alignment.topRight,
+                              child: Icon(
+                                CupertinoIcons.heart,
+                                color: Color(0xFFC72931),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 230,
+                      width: 170,
+                      decoration: BoxDecoration(color: Color(0xFF303A40)),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: Row(
+                  children: [
+                    Container(
+                      height: 230,
+                      width: 170,
+                      decoration: BoxDecoration(color: Color(0xFF303A40)),
+                    ),
+                    Container(
+                      height: 230,
+                      width: 170,
+                      decoration: BoxDecoration(color: Color(0xFF283035)),
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
