10d583ad63ee3d60910a176ff16a1394836d773a
JohnGastone <johngastone11@gmail.com>
2024-06-18 15:35:00 +0300

    +++

---
+++ lib/pages/platePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlatePage extends StatefulWidget {
+  const PlatePage({super.key});
+
+  @override
+  State<PlatePage> createState() => _PlatePageState();
+}
+
+class _PlatePageState extends State<PlatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
